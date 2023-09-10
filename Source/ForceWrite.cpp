ea203ef1704b6df54a4574f9f7bba206eb07d79b
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-09-10 18:33:26 +0200

    Let glibc handle arch differences

--- Source/ForceWrite.cpp
+++ Source/ForceWrite.cpp
@@ -5,9 +5,5 @@ void ForceWrite::write(void* ptr, const char* bytes, std::size_t length)
 	if (procMem == nullptr)
 		procMem = std::make_unique<ProcMem>();
 
-#ifdef __x86_64
-	pwrite64(procMem->fd, bytes, length, reinterpret_cast<off_t>(ptr));
-#else
 	pwrite(procMem->fd, bytes, length, reinterpret_cast<off_t>(ptr));
-#endif
 }
